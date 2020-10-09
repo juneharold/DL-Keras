5329eafc611f85c91de2e32ee9882fc17e849360
Charrybot <junehahwang@gmail.com>
2020-10-09 22:33:01 +0900

    Initial commit

---
+++ Module1/ANN.py
@@ -0,0 +1,9 @@
+import numpy as np # import Numpy library to generate
+
+weights = np.around(np.random.uniform(size=6), decimals=2) # initialize the weights
+biases = np.around(np.random.uniform(size=3), decimals=2) # initialize the biases
+
+x_1 = 0.5 # input 1
+x_2 = 0.85 # input 2
+
+print('x1 is {} and x2 is {}'.format(x_1, x_2))
